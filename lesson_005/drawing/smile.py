5d954c54af35fcedcbf9965ecb6ab86296b31a8d
mikle44 <mike.polynin@gmail.com>
2020-08-27 22:32:31 +0300

    Lesson 5. Task 04 fix needed

--- lesson_005/drawing/smile.py
+++ lesson_005/drawing/smile.py
@@ -3,7 +3,10 @@
 import simple_draw
 
 view = [1]  # TODO почему именно список, а не, к примеру, True/False? Но это не понадобится, лучше использовать две
-            #  глобальные переменные цветов (см. ниже)
+#  глобальные переменные цветов (см. ниже)
+
+color_1 = simple_draw.COLOR_WHITE
+color_2 = simple_draw.COLOR_WHITE
 
 
 def smiles_draw(x, y, color):
@@ -23,11 +26,13 @@ def smiles_draw(x, y, color):
     right_eye_point = simple_draw.get_point(x + 20, y + 10)
     start_point = simple_draw.get_point(x + 10, y + 10)
     end_point = simple_draw.get_point(x + 30, y + 10)
-    if view[0] == 0:
-        simple_draw.line(start_point, end_point, simple_draw.background_color)
-        simple_draw.circle(right_eye_point, 10, color)
-    else:
-        simple_draw.line(start_point, end_point, color)
-        simple_draw.circle(right_eye_point, 10, simple_draw.background_color)
+
+    global color_1
+    global color_2
+
+    simple_draw.line(start_point, end_point, color_1)
+    simple_draw.circle(right_eye_point, 10, color_2)
+    color_1 = color
+    color_2 = simple_draw.background_color
+    color_1, color_2 = color_2, color_1
     # TODO Используйте такуй трюк для смены цветов: color1, color2 = color2, color1 и дублировать код не понадобится
-    view[0] = 0 if view[0] == 1 else view[0] == 0
